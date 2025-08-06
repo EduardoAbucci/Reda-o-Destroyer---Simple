7e514008c12b95b8801a162699a28a61ef603582
procrastination <carlos.abucci.ferraz@escola.pr.gov.br>
2025-08-06 15:32:43 -0300

    Update index.js
    adicionado função de detecção de novas áreas

--- index.js
+++ index.js
@@ -1,37 +1,46 @@
 
 /** Script Simples para burlar o sistema de cola do Redação Paraná  */
 
-let inputField = document.querySelector('input.MuiInputBase-input.MuiOutlinedInput-input.MuiInputBase-inputMultiline.css-1nu4rzx, textarea.MuiInputBase-input.MuiOutlinedInput-input.MuiInputBase-inputMultiline.css-1nu4rzx');
+let inputField;
 
+try {
+    inputField = document.querySelector('input.MuiInputBase-input.MuiOutlinedInput-input.MuiInputBase-inputMultiline.css-1nu4rzx, textarea.MuiInputBase-input.MuiOutlinedInput-input.MuiInputBase-inputMultiline.css-1nu4rzx');
+} catch (error) {
+    inputField = document.querySelector('input, textarea');
+}
+
+console.log(inputField)
 if (inputField) {
-    let notify = prompt("EErdanter the redation");
-
+    let notify = prompt("Enter the redation");
     
-    function simulateKeyPress(char) {
-        let event = new KeyboardEvent('keypress', { key: char });
-        inputField.dispatchEvent(event);
-    }
-
-    
-    function typeText(text) {
-        
-        let currentValue = inputField.value;
-        inputField.value = currentValue; 
-        let index = 0;
-
-        function typeNextChar() {
-            if (index < text.length) {
-                inputField.value += text[index]; 
-                simulateKeyPress(text[index]);  
-                index++;
-                setTimeout(typeNextChar, 100); 
-            }
+    if (notify) {
+        function simulateKeyPress(char) {
+            let event = new KeyboardEvent('keypress', { key: char });
+            inputField.dispatchEvent(event);
         }
-        
-        typeNextChar();
+
+        function typeText(text) {
+            let currentValue = inputField.value;
+            inputField.value = currentValue;
+            let index = 0;
+
+            function typeNextChar() {
+                if (index < text.length) {
+                    inputField.value += text[index];
+                    simulateKeyPress(text[index]);
+                    index++;
+                    setTimeout(typeNextChar, 100);
+                }
+            }
+
+            typeNextChar();
+        }
+
+        typeText(notify);
+    } else {
+        alert('Texto não fornecido no prompt!');
     }
 
-    typeText(notify); 
 } else {
     alert('Nenhum campo de texto encontrado na página!');
 }
